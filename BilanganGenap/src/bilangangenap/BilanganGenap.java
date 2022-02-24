1fbe99b60f796630ab98c24fe165a805b307fea1
MRiswanFauzi <99298203+mrisswan@users.noreply.github.com>
2022-02-24 10:32:04 +0700

    Add files via upload

---
+++ BilanganGenap/src/bilangangenap/BilanganGenap.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bilangangenap;
+
+public class BilanganGenap {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Bilangan genap 0-20");
+        
+       for(int i=0; i <= 20; i++){
+           if (i%2==0)
+            System.out.print(i+",");
+       }
+        
+    }
+    
+}
